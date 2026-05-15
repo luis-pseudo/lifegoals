7955bde10f925c5926098a334b1f14740425b8c5
luis-pseudo <luisangelpr31@gmail.com>
2026-05-15 13:16:46 -0600

    routes working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <nav>
+      <a routerLink="/home" routerLinkActive="active">Home</a> |
+      <a routerLink="/about" routerLinkActive="active">About</a>
+    </nav>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAmGLgSeMGF3itRxF48l4sdpxCDYBdmf0w',
+    authDomain: 'lifegoals-3d36f.firebaseapp.com',
+    projectId: 'lifegoals-3d36f',
+    storageBucket: 'lifegoals-3d36f.firebasestorage.app',
+    messagingSenderId: '130153849376',
+    appId: '1:130153849376:web:00b1296e4e2a7ed2cbd245',
+  }
+};
